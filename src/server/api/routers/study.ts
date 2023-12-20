9e42496af450f46ef92ecaa12563896e406e06ef
Thorsten Thiel <thorsten.thiel@atmina.de>
2023-12-20 17:21:14 +0100

    Allow import and export of study structure

--- src/server/api/routers/study.ts
+++ src/server/api/routers/study.ts
@@ -2,6 +2,15 @@ import {createTRPCRouter, protectedProcedure} from '~/server/api/trpc';
 import {z} from 'zod';
 import {ParticipationEvents} from '~/server/api/routers/participationEvents';
 
+const studyShape = z.object({
+  name: z.string(),
+  durationInMinutes: z.number(),
+  startText: z.string().default(''),
+  startLinkTemplate: z.string().optional().nullable(),
+  endText: z.string().default(''),
+  endLinkTemplate: z.string().optional().nullable(),
+});
+
 export const studyRouter = createTRPCRouter({
   getAll: protectedProcedure.query(({ctx}) => {
     return ctx.prisma.study.findMany({
@@ -18,25 +27,21 @@ export const studyRouter = createTRPCRouter({
   add: protectedProcedure
     .input(
       z.object({
-        study: z.object({
-          name: z.string(),
-          durationInMinutes: z.number(),
-          startText: z.string().default(''),
-          startLinkTemplate: z.string().optional().nullable(),
-          folder: z.array(
-            z.object({
-              name: z.string(),
-              order: z.number().optional(),
-            }),
-          ),
-          email: z.array(
-            z.object({
-              emailId: z.string().nullable(),
-            }),
-          ),
-          endText: z.string().default(''),
-          endLinkTemplate: z.string().optional().nullable(),
-        }),
+        study: studyShape.and(
+          z.object({
+            folder: z.array(
+              z.object({
+                name: z.string(),
+                order: z.number().optional(),
+              }),
+            ),
+            email: z.array(
+              z.object({
+                emailId: z.string().nullable(),
+              }),
+            ),
+          }),
+        ),
       }),
     )
     .mutation(async ({ctx, input}) => {
@@ -77,27 +82,23 @@ export const studyRouter = createTRPCRouter({
     .input(
       z.object({
         id: z.string().uuid(),
-        study: z.object({
-          name: z.string(),
-          durationInMinutes: z.number(),
-          startText: z.string().default(''),
-          startLinkTemplate: z.string().optional().nullable(),
-          endText: z.string().default(''),
-          endLinkTemplate: z.string().optional().nullable(),
-          folder: z.array(
-            z.object({
-              id: z.string().uuid().optional(),
-              name: z.string(),
-              order: z.number(),
-            }),
-          ),
-          email: z.array(
-            z.object({
-              id: z.string().uuid().optional(),
-              emailId: z.string().uuid(),
-            }),
-          ),
-        }),
+        study: studyShape.and(
+          z.object({
+            folder: z.array(
+              z.object({
+                id: z.string().uuid().optional(),
+                name: z.string(),
+                order: z.number(),
+              }),
+            ),
+            email: z.array(
+              z.object({
+                id: z.string().uuid().optional(),
+                emailId: z.string().uuid(),
+              }),
+            ),
+          }),
+        ),
       }),
     )
     .mutation(async ({ctx, input}) => {
@@ -380,4 +381,148 @@ export const studyRouter = createTRPCRouter({
       return emailsExport;
     });
   }),
+  exportStudyStructure: protectedProcedure.input(z.string().uuid()).mutation(async ({ctx, input}) => {
+    const study = await ctx.prisma.study.findUnique({
+      where: {
+        id: input,
+      },
+      include: {
+        folder: true,
+        email: {
+          include: {
+            email: true,
+          },
+        },
+      },
+    });
+
+    if (!study) {
+      throw new Error('Study not found');
+    }
+
+    return {
+      ...study,
+      folder: study.folder.map((f) => {
+        const {studyId, ...folder} = f;
+
+        return {
+          ...folder,
+        };
+      }),
+      email: study.email.map((e) => ({
+        ...e.email,
+      })),
+    };
+  }),
+  importStudy: protectedProcedure
+    .input(
+      studyShape.and(
+        z.object({
+          id: z.string().uuid(),
+          folder: z.array(
+            z.object({
+              id: z.string().uuid(),
+              name: z.string(),
+              order: z.number(),
+            }),
+          ),
+          email: z.array(
+            z.object({
+              id: z.string().uuid(),
+              senderMail: z.string(),
+              senderName: z.string(),
+              subject: z.string(),
+              headers: z.string(),
+              body: z.string(),
+              backofficeIdentifier: z.string(),
+            }),
+          ),
+        }),
+      ),
+    )
+    .mutation(async ({ctx, input}) => {
+      return ctx.prisma.$transaction(async (tx) => {
+        const study = await tx.study.upsert({
+          where: {
+            id: input.id,
+          },
+          update: {
+            name: input.name,
+            durationInMinutes: input.durationInMinutes,
+            startText: input.startText,
+            startLinkTemplate: input.startLinkTemplate,
+            endText: input.endText,
+            endLinkTemplate: input.endLinkTemplate,
+          },
+          create: {
+            id: input.id,
+            name: input.name,
+            durationInMinutes: input.durationInMinutes,
+            startText: input.startText,
+            startLinkTemplate: input.startLinkTemplate,
+            endText: input.endText,
+            endLinkTemplate: input.endLinkTemplate,
+          },
+          select: {
+            folder: true,
+            email: true,
+          },
+        });
+
+        await tx.folder.deleteMany({
+          where: {
+            studyId: input.id,
+            id: {
+              notIn: input.folder.map((f) => f.id),
+            },
+          },
+        });
+
+        for (const folder of input.folder) {
+          await tx.folder.upsert({
+            where: {
+              id: folder.id,
+            },
+            update: {
+              ...folder,
+              studyId: input.id,
+            },
+            create: {
+              ...folder,
+              studyId: input.id,
+            },
+          });
+        }
+
+        for (const email of input.email) {
+          await tx.email.upsert({
+            where: {
+              id: email.id
+            },
+            update: email,
+            create: email
+          });
+        }
+
+        await tx.studyEmail.deleteMany({
+          where: {
+            studyId: input.id,
+            emailId: {
+              notIn: input.email.map((e) => e.id),
+            },
+          },
+        });
+
+        await tx.studyEmail.createMany({
+          data: input.email
+            .filter(email => !study.email.some(e => e.emailId == email.id))
+            .map((e) => {
+              return {
+                studyId: input.id,
+                emailId: e.id,
+              }
+            })
+        });
+      });
+    }),
 });
