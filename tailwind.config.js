9cde864f365af25d5c8db754b7002315e8e7d3c7
Thorsten Thiel <thorsten.thiel@atmina.de>
2024-12-11 10:28:18 +0100

    Flash finish button when visible

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,35 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        highlight: {
+          '0%': {
+            '-webkit-transform': 'scale(0.9)',
+            '-ms-transform': 'scale(0.9)',
+            'transform': 'scale(0.9)'
+          },
+          '25%': {
+            '-webkit-transform': 'scale(1)',
+            '-ms-transform': 'scale(1)',
+            'transform': 'scale(1)'
+          },
+          '60%': {
+            '-webkit-transform': 'scale(0.9)',
+            '-ms-transform': 'scale(0.9)',
+            'transform': 'scale(0.9)'
+          },
+          '100%': {
+            '-webkit-transform': 'scale(0.9)',
+            '-ms-transform': 'scale(0.9)',
+            'transform': 'scale(0.9)'
+          }
+        }
+      },
+      animation: {
+        highlight: 'highlight 2s ease-in-out infinite'
+      }
+    },
   },
   plugins: [],
 };
